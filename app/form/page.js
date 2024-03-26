7f595e1a98bc3bf2edb6d2c0a37e613f55af2b9c
Ezomon Glory <ezomonglory01@gmail.com>
2024-03-26 22:02:50 +0100

    fixed deployment bug

--- app/form/page.js
+++ app/form/page.js
@@ -1,13 +1,13 @@
 "use client";
 import Btn from "@/components/Btn";
-import FormLayout from "@/components/FormLAyout";
+import FormLayout from "@/components/FormLayout";
 import TextInput from "@/components/TextInput";
 import React from "react";
 
 const Page = () => {
 	return (
 		<FormLayout>
-			<div>                
+			<div>
 				<div className='bg-white rounded-[12px] py-[48px] px-[51px] w-[60%] max-w-[437px] mx-auto border border-[#E6E8EC] '>
 					<h2 className='inter700 text-[1.5rem] leading-[36px] text-black '>
 						Application Form
