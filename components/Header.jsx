949ad345601eac4ca2c891242552418718a21198
Moses-Kelvin <moseskelvin683@gmail.com>
2024-06-17 16:55:02 +0100

    fix header

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,10 +14,13 @@ const Header = () => {
 
 	return (
 		<div className="hidden lg:block w-full px-[3.75rem] pt-[1rem]">
-			<div className="flex justify-between w-full">
+			<div className="flex flex-col w-full">
+				<Link className="self-end" href="/signin">
+					<Btn text="Sign in" />
+				</Link>
 				<Link
 					href="/"
-					className="flex text-center font-bold flex-col space-y-[10px] mx-auto"
+					className="flex text-center font-bold flex-col space-y-[10px] mx-auto -translate-y-4"
 				>
 					<img alt="" className="self-center w-[100px] h-[90px]" src="/images/logo.svg" />
 					<h2 className="sf600 text-[18px] leading-[12px] uppercase text-[#68768C]">
@@ -26,15 +29,13 @@ const Header = () => {
 					<div className="text-[#1B7339] space-y-[10px]">
 						<h2 className="sf600 text-[18px] leading-[12px] uppercase">
 							OCCUPATIONAL SAFETY & HEALTH DEPARTMENT
-					</h2>
+						</h2>
 						<h2 className="sf600 text-[18px] leading-[12px] uppercase">
 							FEDERAL MINISTRY OF LABOUR & EMPLOYMENT
-					</h2>
+						</h2>
 					</div>
 				</Link>
-				<Link className="" href="/signin">
-					<Btn text="Sign in" />
-				</Link>
+
 			</div>
 
 			<div className='mt-[3rem] flex space-x-[2rem] justify-center w-full'>
